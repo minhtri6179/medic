c04f02340a9057dde7da66383a31823e0b75c9ee
Sagar2k1 <84672270+Sagar2k1@users.noreply.github.com>
2022-11-24 14:45:11 +0700

    cap nhat lai giao dien

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+c = Client()
+response = c.get('/login/', {'username': 'lam123', 'password': '123456'})
+response.status_code
--- prescribing_management/tests.py
+++ prescribing_management/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import unittest
+from django.test import TestCase,Client
+from selenium.webdriver import Chrome, ChromeOptions
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 # Create your tests here.
+class LoginTest(StaticLiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element(By.NAME, "username")
+        username_input.send_keys('lam123')
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys('123456')
+        self.selenium.find_element(By.XPATH, '/input[@value="Log in"]').click()
+
+

95f8579faf6d612d2ad8f1c89df1a865fed745c1
mchi11 <trinm110@gmail.com>
2022-11-30 14:49:12 +0700

    hide secret key

--- prescribing/settings.py
+++ prescribing/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$z&7@9b$2yy#dsmqmkoseeuzr0(^csk2#7zzsb5t@tdu&d-(v^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '*']
-
+ALLOWED_HOSTS = ['medic-production.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://medic-production.up.railway.app']
 
 # Application definition
 MEDIA_URL = '/media/'
